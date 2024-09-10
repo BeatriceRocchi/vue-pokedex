b4e3079cb6f4b79044bb6131d326fbb34caf661a
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-09-10 15:16:10 +0200

    [add] pokeapi with console.log of the result

---
+++ src/components/Main.vue
@@ -0,0 +1,35 @@
+<script>
+import Search from "./partials/Search.vue";
+
+export default {
+  components: {
+    Search,
+  },
+};
+</script>
+
+<template>
+  <main id="pokedex">
+    <!-- Left side: pokemon searchbar, pokemon catch/release and pokemon info -->
+    <div>
+      <Search />
+    </div>
+    <!-- Right side: my pokemon list -->
+    <div>RIGHT</div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/partials/variables";
+#pokedex {
+  background-color: $color-pokedex;
+  margin: 100px auto;
+  height: calc(100vh - 300px);
+  width: 60%;
+  display: flex;
+
+  div {
+    width: 50%;
+  }
+}
+</style>
---
+++ src/components/partials/Search.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      pokemonToSearch: "",
+      apiUrl: "https://pokeapi.co/api/v2/pokemon/",
+    };
+  },
+  methods: {
+    searchPokemon() {
+      console.log(this.pokemonToSearch);
+      axios
+        .get(this.apiUrl + this.pokemonToSearch)
+        .then((result) => {
+          console.log(result.data.name);
+          console.log(result.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<template>
+  <input type="text" v-model="pokemonToSearch" />
+  <button @click="searchPokemon">Find</button>
+</template>
+
+<style lang="scss" scoped></style>
