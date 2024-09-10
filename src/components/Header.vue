fa9173d7298deafd20d13b3a8166dfb16d04195e
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-09-10 14:41:33 +0200

    [add] scaffolding and [add] header component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <header>
+    <h1>Pokedex</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/partials/variables.scss";
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: $color-primary;
+}
+</style>
